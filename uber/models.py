da44fa8a9f1e2e78dfeb35e4b214e789025be4d5
zecollokaris <collo.kariss@gmail.com>
2018-08-03 12:59:10 +0300

    created class driver

--- uber/models.py
+++ uber/models.py
@@ -6,3 +6,18 @@ from django.contrib.auth.models import User
 #---------------------------------------------------------------------#
 
 
+# MODELS CREATED HERE!
+
+#################################################################################################################################################################################
+# MODEL FOR DRIVER(UBER)!
+#################################################################################################################################################################################
+
+#...Class DRIVER added here...
+class Driver(models.Model):
+#Attribute Variables for Driver class to represent different columns in database
+    '''
+    name-: This is the name of the driver 
+    avatar-: A picture in the profile to show the rider how the driver looks like
+    car-: This is the car that will be used to pick up the rider connected to Car class using a FOREIGN-KEY
+    pickup_location-: Connected to the Location class using a FOREIGN_KEY
+    '''
