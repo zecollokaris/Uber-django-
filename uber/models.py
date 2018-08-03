5d5ca69daff842c5c75a608ea31499fc7b1d37ae
zecollokaris <collo.kariss@gmail.com>
2018-08-03 12:59:41 +0300

    added class attributes for driver class

--- uber/models.py
+++ uber/models.py
@@ -21,3 +21,15 @@ class Driver(models.Model):
     car-: This is the car that will be used to pick up the rider connected to Car class using a FOREIGN-KEY
     pickup_location-: Connected to the Location class using a FOREIGN_KEY
     '''
+    name = models.OneToOneField(User,on_delete=models.CASCADE)
+    bio = models.CharField(max_length=60)
+    avatar = models.ImageField(upload_to='ProfilePicture/')
+    vehicle = models.ForeignKey('uber.Car', on_delete=models.CASCADE)
+    pickup_location = models.ForeignKey('uber.Location', on_delete=models.CASCADE)
+    contact_info = models.CharField(max_length=50)
+
+    
+    '''Method to filter database results'''
+    def __str__(self):
+        return self.user.username 
+
