de3c836f4a9ce3d0ae751ba0d9b2634cc9d31b24
zecollokaris <collo.kariss@gmail.com>
2018-08-03 13:09:10 +0300

    started on passenger models now created passenger class

--- passenger/models.py
+++ passenger/models.py
@@ -5,3 +5,20 @@ from uber.models import Location
 from django.contrib.auth.models import User
 '''End Of Import'''
 #---------------------------------------------------------------------#
+
+
+
+# MODELS CREATED HERE!
+
+#################################################################################################################################################################################
+# MODEL FOR PASSENGERS!
+#################################################################################################################################################################################
+
+#...Class DRIVER added here...
+class Passenger(models.Model):
+#Attribute Variables for Driver class to represent different columns in database
+    '''
+    name-: This is the name of the passenger
+    avatar-: A picture of the rider
+    pickup_location-: Connected to the Location class using a FOREIGN_KEY
+    '''
