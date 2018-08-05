314b5e4fd41feb959526aeeaee0d28fdeb5a3ad1
zecollokaris <collo.kariss@gmail.com>
2018-08-05 18:53:49 +0300

    done with imports for driver views

--- uber/views.py
+++ uber/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+from django.conf.urls import url,include
+
+'''End Of Import'''
+#---------------------------------------------------------------------#
