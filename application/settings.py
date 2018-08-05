95f6a108c96bde497952f37f10248c872661f96d
zecollokaris <collo.kariss@gmail.com>
2018-08-05 20:21:15 +0300

    enabled static files & templates to be found in root folder

--- application/settings.py
+++ application/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'application.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#enables static files to be found in root folder 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    
+]
+
+LOGIN_REDIRECT_URL = '/driver/home'
